64a01a0325159000694755e379c5822f24c4bce1
ɪᴋɢᴍᴏɴxʀ <imgmtt@gmail.com>
2026-07-15 17:05:49 -0600

    Update HUB.lua

--- HUB.lua
+++ HUB.lua
@@ -14,22 +14,22 @@ local Scripts = {
 
     [120766736586332] = function()  -- +1 Strength Per Click
         print("🎮 +1 Fuerza por Clic detectado → Ejecutando...")
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/imgmtt-bot/Speed/refs/heads/main/Fuerzaxclick.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ikgmonxr/Speed/refs/heads/main/Fuerzaxclick.lua"))()
     end,
 
     [116223724643557] = function()  -- +1 Magic Evolution
         print("🎮 +1 Evolución Mágica detectado → Ejecutando...")
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/imgmtt-bot/Speed/refs/heads/main/Evolucion.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ikgmonxr/Speed/refs/heads/main/Evolucion.lua"))()
     end,
 
     [86378115369061] = function()   -- +1 Backflip Obby Escape
         print("🎮 +1 Backflip Obby detectado → Ejecutando...")
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/imgmtt-bot/Speed/refs/heads/main/Speed.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ikgmonxr/Speed/refs/heads/main/Speed.lua"))()
     end,
 
     [99457855080430] = function()   -- +1 Speed Barrel Escape (Nuevo)
         print("🎮 Escapa del Barril de Velocidad detectado → Ejecutando...")
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/imgmtt-bot/Speed/refs/heads/main/Escapabarril.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ikgmonxr/Speed/refs/heads/main/Escapabarril.lua"))()
     end,
 }
 
