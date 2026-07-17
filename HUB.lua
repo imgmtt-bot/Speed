6853c645fc2abfc15a147c434e77efd3be5dbcd5
ɪᴋɢᴍᴏɴxʀ <imgmtt@gmail.com>
2026-07-16 23:46:15 -0600

    Add new game detection and update comments
    Updated game detection script to include new games and modified comments.

--- HUB.lua
+++ HUB.lua
@@ -1,6 +1,6 @@
 -- =============================================== \\
 -- // GAME DETECTOR + SCRIPT LOADER
--- // 6 juegos agregados
+-- // 7 juegos agregados
 -- =============================================== \\
 local PlaceId = game.PlaceId
 local Players = game:GetService("Players")
@@ -24,21 +24,25 @@ local Scripts = {
         loadstring(game:HttpGet("https://raw.githubusercontent.com/imgmtt-bot/Speed/refs/heads/main/Speed.lua"))()
     end,
 
-    [99457855080430] = function() -- +1 Speed Barrel Escape (Nuevo)
+    [99457855080430] = function() -- +1 Speed Barrel Escape
         print("🎮 Escapa del Barril de Velocidad detectado → Ejecutando...")
         loadstring(game:HttpGet("https://raw.githubusercontent.com/imgmtt-bot/Speed/refs/heads/main/Escapabarril.lua"))()
     end,
 
-    -- ================== NUEVO ==================
     [109509029034984] = function() -- +1 Aura Per Click
         print("🎮 +1 Aura Por Clic detectado → Ejecutando...")
         loadstring(game:HttpGet("https://raw.githubusercontent.com/ikgmonxr/Speed/refs/heads/main/Auraxclick"))()
     end,
 
-    -- ================== NUEVO 2 ==================
     [115196082055466] = function() -- Shoot The Boss
         print("🎮 Shoot The Boss detectado → Ejecutando...")
         loadstring(game:HttpGet("https://raw.githubusercontent.com/ikgmonxr/Speed/refs/heads/main/Dispara"))()
+    end,
+
+    -- ================== NUEVO ==================
+    [124868078719468] = function() -- Climb a Ramp
+        print("🎮 Climb a Ramp detectado → Ejecutando...")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ikgmonxr/Speed/refs/heads/main/Sube.lua"))()
     end,
 }
 
